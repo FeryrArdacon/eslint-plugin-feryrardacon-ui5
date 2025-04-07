3958bc905e736e9b3b03034a0bcb851f79c471fe
Patrick Siegmund <patrick.siegmund@live.de>
2025-04-07 17:01:40 +0200

    Komplett überarbeitung der Logik

--- rules/hungarian-notation.js
+++ rules/hungarian-notation.js
@@ -12,10 +12,6 @@ module.exports = {
       {
         type: "object",
         properties: {
-          onlyDeclarations: {
-            type: "boolean",
-            default: false,
-          },
           ignore: {
             type: "array",
             items: {
@@ -31,51 +27,145 @@ module.exports = {
   create: function (oContext) {
     const sErrorMsg = "Variable not named after hungarian notation";
 
-    const aTypes = [
-      "VariableDeclarator",
-      "FunctionExpression",
-      "FunctionDeclaration",
-    ];
-
-    const aRegExpChecks = [
+    const aRegExpChecksVariable = [
       // allow hungarian notation (with snakeCase) for declarations
-      /^_?(\$|fn|[a-z]|)([A-Z]+[a-z0-9]*)+$/,
-      // allow PascalCase e.g. for class names
-      /^(_?[A-Z]+[a-z0-9]*)+$/,
+      /^_?(\$|fn|[a-z])([A-Z]+[a-z0-9]*)+$/,
       // allow upper snake case for constants
       /^([A-Z0-9]+_*)+$/,
     ];
 
+    const aRegExpChecksParameter = [
+      // allow pascal case for function parameters
+      /^([A-Z][a-zA-Z0-9]*)+$/,
+      ...aRegExpChecksVariable,
+    ];
+
     const oOptions = oContext.options[0] || {};
-    const bOnlyDeclarations = !!oOptions.onlyDeclarations;
     const aIgnore = oOptions.ignore || [];
 
     // allow some special cases
     let aWhitelist = ["[a-z]", "resolve", "reject", "sap", "jQuery"];
     aWhitelist = aWhitelist.concat(aIgnore);
 
-    aRegExpChecks.push(new RegExp("^(" + aWhitelist.join("|") + ")$"));
+    aRegExpChecksVariable.push(new RegExp("^(" + aWhitelist.join("|") + ")$"));
+    aRegExpChecksParameter.push(new RegExp("^(" + aWhitelist.join("|") + ")$"));
+
+    const oDeclarations = {
+      oVariable: {
+        TYPE: "VariableDeclaration",
+        getNames(oNode) {
+          return oNode.declarations
+            .map((oChildNode) =>
+              oChildNode?.type === "VariableDeclarator" &&
+              oChildNode?.init?.type !== "ArrowFunctionExpression" &&
+              oChildNode?.init?.type !== "FunctionExpression"
+                ? oChildNode?.id?.name
+                : null
+            )
+            .filter(Boolean);
+        },
+        isRequire(oNode) {
+          return oNode.declarations.some((oChildNode) => {
+            return (
+              oChildNode.type === "CallExpression" &&
+              oChildNode.callee &&
+              oChildNode.callee.name === "require"
+            );
+          });
+        },
+        checkNames(oNode) {
+          return !oDeclarations.oVariable.isRequire(oNode)
+            ? oDeclarations.oVariable
+                .getNames(oNode)
+                .reduce((aIncorrectNames, sName) => {
+                  if (
+                    aRegExpChecksVariable.some((oRegExp) =>
+                      oRegExp.test(sName)
+                    ) === false
+                  ) {
+                    aIncorrectNames.push(sName);
+                  }
+                  return aIncorrectNames;
+                }, [])
+            : [];
+        },
+      },
+      oFunction: {
+        TYPE: "FunctionDeclaration",
+        getNames(oNode) {
+          return oNode.params
+            .map(
+              (oChildNode) =>
+                oChildNode?.name ||
+                oChildNode?.id?.name ||
+                oChildNode?.left?.name ||
+                oChildNode?.argument?.name
+            )
+            .filter(Boolean);
+        },
+        checkNames(oNode) {
+          return oDeclarations.oFunction
+            .getNames(oNode)
+            .reduce((aIncorrectNames, sName) => {
+              if (
+                aRegExpChecksParameter.some((oRegExp) =>
+                  oRegExp.test(sName)
+                ) === false
+              ) {
+                aIncorrectNames.push(sName);
+              }
+              return aIncorrectNames;
+            }, []);
+        },
+      },
+    };
+
+    const oExpressions = {
+      oFunction: {
+        TYPE: "FunctionExpression",
+        checkNames: oDeclarations.oFunction.checkNames,
+      },
+      oArrowFunction: {
+        TYPE: "ArrowFunctionExpression",
+        checkNames(oNode) {
+          return oDeclarations.oFunction
+            .getNames(oNode)
+            .reduce((aIncorrectNames, sName) => {
+              if (
+                aRegExpChecksVariable.some((oRegExp) => oRegExp.test(sName)) ===
+                false
+              ) {
+                aIncorrectNames.push(sName);
+              }
+              return aIncorrectNames;
+            }, []);
+        },
+      },
+    };
+
+    const reportNames = (oNode, aIncorrectNames, sRule) => {
+      aIncorrectNames.forEach((sName) => {
+        oContext.report(oNode, `'${sName}': ${sErrorMsg} [${sRule}]`);
+      });
+    };
 
     return {
-      Identifier(oNode) {
-        const sName = oNode.name;
-        const oParent =
-          oNode.parent.type === "MemberExpression"
-            ? oNode.parent.parent
-            : oNode.parent;
-
-        if (!aTypes.some((sType) => sType === oNode.parent.type)) return;
-
-        const isDeclaration =
-          (oParent.type === "FunctionDeclaration" &&
-            oParent.id.name === sName) ||
-          oParent.type === "VariableDeclarator";
-
-        if (isDeclaration || !bOnlyDeclarations) {
-          if (!aRegExpChecks.some((r) => r.test(sName))) {
-            oContext.report(oNode, sErrorMsg);
-          }
-        }
+      VariableDeclaration(oNode) {
+        const aIncorrectNames = oDeclarations.oVariable.checkNames(oNode) || [];
+        reportNames(oNode, aIncorrectNames, "VariableDeclaration");
+      },
+      FunctionDeclaration(oNode) {
+        const aIncorrectNames = oDeclarations.oFunction.checkNames(oNode) || [];
+        reportNames(oNode, aIncorrectNames, "FunctionDeclaration");
+      },
+      FunctionExpression(oNode) {
+        const aIncorrectNames = oExpressions.oFunction.checkNames(oNode) || [];
+        reportNames(oNode, aIncorrectNames, "FunctionExpression");
+      },
+      ArrowFunctionExpression(oNode) {
+        const aIncorrectNames =
+          oExpressions.oArrowFunction.checkNames(oNode) || [];
+        reportNames(oNode, aIncorrectNames, "ArrowFunctionExpression");
       },
     };
   },
